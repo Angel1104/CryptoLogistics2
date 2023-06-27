d0d169d5e85b4de9664e2bff8f465e72478a698a
Angel1104 <angel@DESKTOP-2K06226>
2023-06-26 20:09:40 -0400

    final

--- Components/Services.jsx
+++ Components/Services.jsx
@@ -9,9 +9,9 @@ const Services = ({
 
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-2 gap-x-9 gap-y-10 mx-6">
+    <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 xl:grid-cols-2 gap-x-9 gap-y-10 mx-6 ">
       <div 
-      className="hover:cursor-pointer h-32 flex flex-col bg-gray-800 drop-shadow hover:drop-shadow-lg hover:opacity-70"
+      className="hover:cursor-pointer h-32 flex flex-col bg-gray-800 drop-shadow hover:drop-shadow-lg hover:opacity-70 mr-5"
       onClick={()=>setCreateShipmentModel(true)}
       >
         <div className="flex justify-center items-center h-screen">
@@ -25,7 +25,7 @@ const Services = ({
       </div>
       <div 
       onClick={()=>setStartModal(true)}
-      className="hover:cursor-pointer h-32 flex flex-col bg-gray-800 drop-shadow hover:drop-shadow-lg hover:opacity-70">
+      className="hover:cursor-pointer h-32 flex flex-col bg-gray-800 drop-shadow hover:drop-shadow-lg hover:opacity-70 ml-5">
         <div className="flex justify-center items-center h-screen">
           <p className="mr-3 text-white">2</p>
           <button className="text-white">
@@ -43,7 +43,7 @@ const Services = ({
       </div> */}
       <div 
       onClick={()=>setCompleteModal(true)}
-      className="hover:cursor-pointer h-32 flex flex-col bg-gray-800 drop-shadow hover:drop-shadow-lg hover:opacity-70">
+      className="hover:cursor-pointer h-32 flex flex-col bg-gray-800 drop-shadow hover:drop-shadow-lg hover:opacity-70 mr-5">
         <div className="flex justify-center items-center h-screen">
           <p className="mr-3 text-white">3</p>
           <button className="text-white">
